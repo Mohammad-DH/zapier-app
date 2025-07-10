cc4d22299daf0834dd3b199c47dcc542c0b2e165
mrj <mohammad.dehghani@sesami.co>
2025-07-10 03:51:14 +0330

    move to production

--- server/src/sesami/config/Build.config.ts
+++ server/src/sesami/config/Build.config.ts
@@ -14,5 +14,5 @@ export default {
     sesamiClientId: String(process.env.SESAMI_CLIENT_ID),
     sesamiClientSecret: String(process.env.SESAMI_CLIENT_SECRET),
     baseUrl: normalizedURL(String(process.env.APP_DOMAIN)),
-    isOAuthEnable: false, // change to true in production or when you want to test oauth flow
+    isOAuthEnable: true,
 };
